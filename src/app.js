67591b29d149a251794ee1b56c2d07ddc2c1aec4
fy-st0rm <slokpradhan2062@gmail.com>
2023-06-22 22:45:52 +0545

    Ready! Set! Go!

---
+++ src/app.js
@@ -0,0 +1,29 @@
+// Dependencies
+const express = require("express");
+const path    = require("path");
+const logger  = require("morgan");
+const http    = require("http");
+
+// Routers
+const index_router = require("./routes/index");
+
+// Express app
+const app = express();
+const port = 3000;
+
+// Setting up view engine
+app.use(logger('dev'));
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+app.set("port", port);
+
+// Setting up application settings
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Setting up routers
+app.use("/", index_router);
+
+// Running
+const server = http.createServer(app);
+server.listen(port);
